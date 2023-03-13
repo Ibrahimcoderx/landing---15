246c88a0c73b1e37d5be4d7997ab48824eae69df
Ibrahim <ibrahimcoderx@gmail.com>
2023-03-13 20:20:12 +0600

    update

--- Components/theme_1/CustomPlugins/pluginOne.js
+++
@@ -1,8 +0,0 @@
-import Home from "../../theme_1/Login"
-function myPluginOne(editor){
-    editor.BlockManager.add('my-first-block', {
-      label: 'HOME PAGE',
-      content: "<h1>Hellooo</h1>",
-    });
-}
-export default myPluginOne;
